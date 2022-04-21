1011196f0fd5c360abd0eba234124df4a42ad5a4
Doosies <song961003@gmail.com>
2022-04-21 15:30:28 +0900

    여러개 해놨던거 map으로 정리

--- front/src/components/WindowTopbar.tsx
+++ front/src/components/WindowTopbar.tsx
@@ -1,13 +1,9 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import CircleButton, { ButtonDoing, ButtonSize } from './CircleButton';
+import CircleButton from './CircleButton';
 import {FiMaximize2, FiMinus, } from 'react-icons/fi'
 import {AiOutlineClose} from 'react-icons/ai'
-// import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
-
-interface WindowTopbarProps {
-    
-}
+import { ButtonDoing, ButtonSize } from '../enum/buttonEnum';
 
 const WindowTopbarBlock = styled.div`
     width: 100%;
@@ -30,17 +26,16 @@ const WindowControllContainer = styled.div`
     justify-content: space-around;
 `;
 
+const circles = [
+    {color: '#FF5F57', size: ButtonSize.small, doing: ButtonDoing.close, strokeWidth: 90, icon: AiOutlineClose},
+    {color: '#FEBC30', size: ButtonSize.small, doing: ButtonDoing.minimize, strokeWidth: 5, icon: FiMinus},
+    {color: '#25C93E', size: ButtonSize.small, doing: ButtonDoing.maxmize, strokeWidth: 2.8, icon: FiMaximize2},
+]
+
 const WindowTopbar = () => {
     const [isOnElement, setOnElement] = useState(false);
-
-    const handleMouseMove = () => {
-        setOnElement(true);
-        console.log("들어왔어");
-    }
-    const handleMouseLeave = () => {
-        setOnElement(false);
-        console.log("나갔어");
-    }
+    const handleMouseMove = () => void setOnElement(true);
+    const handleMouseLeave = () => void setOnElement(false);
 
     return (
         <WindowTopbarBlock>
@@ -48,15 +43,16 @@ const WindowTopbar = () => {
                 onMouseMove={handleMouseMove} 
                 onMouseLeave={handleMouseLeave}
             >
-                <CircleButton color='#FF5F57' size={ButtonSize.small} isHover={isOnElement} doing={ButtonDoing.close}>
-                    <AiOutlineClose strokeWidth={90}/>
-                </CircleButton>
-                <CircleButton color='#FEBC30' size={ButtonSize.small} isHover={isOnElement} doing={ButtonDoing.minimize}>
-                    <FiMinus strokeWidth={5}/>
-                </CircleButton>
-                <CircleButton color='#25C93E' size={ButtonSize.small} isHover={isOnElement} doing={ButtonDoing.maxmize}>
-                    <FiMaximize2 strokeWidth={2.8}/>
-                </CircleButton>
+                {circles.map(el => 
+                    <CircleButton
+                        color={el.color}
+                        size={el.size}
+                        isHover={isOnElement}
+                        doing={el.doing}
+                    >
+                        <el.icon strokeWidth={el.strokeWidth}/>
+                    </CircleButton>
+                )}
             </WindowControllContainer>
         </WindowTopbarBlock>
     );
