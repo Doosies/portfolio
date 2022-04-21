aefa733603e6a5ff76ae05e8bc5200a5e48f8bff
Doosies <song961003@gmail.com>
2022-04-21 15:30:59 +0900

    icon 받아오는 함수 정의

---
+++ front/src/utility/Icons.ts
@@ -0,0 +1,16 @@
+import { IconType } from "react-icons/lib";
+import { ApplicationTypes } from "../enum/applicationTypes";
+
+
+import { RiFinderLine, RiProfileLine, RiMailSendLine, RiTerminalBoxLine } from "react-icons/ri";
+
+function getIconFromAppType(appType: ApplicationTypes):IconType {
+    switch (appType) {
+        case ApplicationTypes.finder: return RiFinderLine;
+        case ApplicationTypes.profile: return RiProfileLine;
+        case ApplicationTypes.sendmail: return RiMailSendLine;
+        case ApplicationTypes.terminal: return RiTerminalBoxLine;
+    }
+}
+
+export default getIconFromAppType;
