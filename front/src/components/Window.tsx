0c9c85c50d80a32a2a12bd4a126c508806544f77
Doosies <song961003@gmail.com>
2022-04-23 13:11:01 +0900

    기본 ui 버그 모두 수정

--- front/src/components/Window.tsx
+++ front/src/components/Window.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import WindowMainPage from './WindowMainPage';
 import WindowTopbar from './WindowTopbar';
-import Draggable from './Draggable';
 import { ApplicationTypes } from '../enum/applicationTypes';
 import { useAppDispatch, useAppSelector } from '../modules/hooks';
 import { mouseDownWindow } from '../modules/window';
@@ -17,70 +16,82 @@ interface WindowProps {
 const WindowBlock = styled.div`
     background-color: #262628;
     position: absolute;
+    border-radius: 20px;
+    box-shadow: -5px 5px 20px -10px black;
 
-    /* transition: all 10s ease-in-out; */
-    /* transform: scale(1.1); */
-    /* transform-origin: 50% 50%; */
-
-
-    @media screen and (max-width: 479px){
-        height: 100%;
-        width: 100%;
+    @media screen and (max-width: 1023px){
+        height: 80%;
+        width: 90%;
     }
-    @media screen and (min-width: 480px) {
-        height: 700px;
-        width: 60%;
-
-        bottom: 20%;
-        border-radius: 20px;
+    @media screen and (min-width: 1024px) {
+        height: 80%;
+        width: 800px;
     }   
 `
 
 const Window = ({
     windowName, windowType, windowId, position
 }:WindowProps) => {
-    // console.log("window reder id: " + windowId);
-    // const handleMouseUp = (e: React.MouseEvent<HTMLDivElement>) => {
     const dispatch = useAppDispatch();
     const window = useAppSelector(state => state.window.windowList.find( el => el.windowId === windowId));
-
-    // const handleClickWindow = () => {
-        
-    // }
 
     const myRef = useRef<HTMLDivElement>(null);
     const [state,setState] = useState({
         isClick: false,
         startX: 0,
         startY: 0,
-        fixedX: myRef.current?.getBoundingClientRect().left,
-        fixedY: myRef.current?.getBoundingClientRect().top
+        left: myRef.current?.getBoundingClientRect().left,
+        top: myRef.current?.getBoundingClientRect().top
     });
     
     const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
         
         dispatch(mouseDownWindow(windowId));
+
         setState({
             ...state,
             isClick: true,
             startX: e.clientX,
             startY: e.clientY
         });
-        // if (onMouseDown) onMouseDown();
     }
     const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
         if (state.isClick) {
-            // 마우스 좌표 (마우스 움직인 거리) + (현재 element의 위치)
-            let x = (e.clientX - state.startX) + (e.currentTarget.getBoundingClientRect().left);
-            let y = (e.clientY - state.startY) + (e.currentTarget.getBoundingClientRect().top);
+            const el = e.currentTarget;
 
+            // 현재 객체의 left, top 값 (마우스 움직인 거리) + (현재 element의 위치)
+            let left = (e.clientX - state.startX) + (e.currentTarget.getBoundingClientRect().left);
+            let top = (e.clientY - state.startY) + (e.currentTarget.getBoundingClientRect().top);
+
+            // 부모 엘레멘트
+            const parent = e.currentTarget.parentElement;
+            // 현재 객체의 실제 x, y 좌표
+            const componentX = el.getBoundingClientRect().x;
+            const componentY = el.getBoundingClientRect().y;
+            
+            console.log(componentY);
+            if (parent) {
+                // // 오른쪽을 벗어나면
+                // if (componentX + el.clientWidth > parent.clientWidth -10) {
+                //     left = parent.clientWidth - el.clientWidth -10;
+                // // 왼쪽을 벗어나면
+                // }else if (componentX < 10) {
+                //     left = 10.1;
+                // // 아래쪽을 벗어나면
+                // }else if (componentY + el.clientHeight > parent.parentElement!.clientHeight -10) {
+                //     top = parent.parentElement!.clientHeight - el.clientHeight - 10;
+                // // 윗쪽을 벗어나면
+                if (componentY < 50) {
+                    top = 50;
+                }
+            }
             setState({
                 ...state,
-                fixedX: x,
-                fixedY: y,
+                left,
+                top,
                 startX: e.clientX,
                 startY: e.clientY,
-            });
+            })
         }
     }
     const handleMouseUp = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -94,7 +105,7 @@ const Window = ({
             onMouseDown={handleMouseDown} 
             onMouseMove={handleMouseMove}
             onMouseLeave={handleMouseUp}
-            style={{left: `${state.fixedX}px`, top: `${state.fixedY}px`, zIndex: `${window?.zindex}`}}
+            style={{left: `${state.left}px`, top: `${state.top}px`, zIndex: `${window?.zindex}`}}
         >
             <WindowTopbar 
                 windowId={windowId}
