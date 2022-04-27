f6e3599a81eefe097c9771f0af5798c2568269e0
Doosies <song961003@gmail.com>
2022-04-27 23:18:29 +0900

    프로필 디테일 애니메이션 변경

--- front/src/components/pages/profile/ProfileNameDetail.tsx
+++ front/src/components/pages/profile/ProfileNameDetail.tsx
@@ -8,11 +8,10 @@ interface ProfileNameDetailProps {
 const ProfileNameDetailBlock = styled.div`
     text-align: center;
     width: 100%;
-    height: 170px;
+    height: 180px;
 
-    font-size: 23px; 
+    font-size: 30px; 
     transform: translateY(-40%);
-    user-select: none;
 
     animation: downAnimation 1s ease-in-out 4s;
     animation-fill-mode: forwards;
@@ -20,15 +19,15 @@ const ProfileNameDetailBlock = styled.div`
     p {
         opacity: 0;
         margin: 0;
-        &:nth-child(2){ animation: fadefadeInDownIn10 1s; animation-fill-mode:forwards;}
-        &:nth-child(3){ animation: fadefadeInDownIn10 1s ease-in-out 1s; animation-fill-mode:forwards;}
-        &:nth-child(4){ animation: fadefadeInDownIn10 1s ease-in-out 2s; animation-fill-mode:forwards;}
+        &:nth-child(2){ animation: fadefadeInDownIn10 0.7s; animation-fill-mode:forwards;}
+        &:nth-child(3){ animation: fadefadeInDownIn10 0.7s ease-in-out 1.4s; animation-fill-mode:forwards;}
+        &:nth-child(4){ animation: fadefadeInDownIn10 0.7s ease-in-out 2.1s; animation-fill-mode:forwards;}
 
-        &:first-child  { animation: fadefadeInDownIn10 1s ease-in-out 3s; animation-fill-mode:forwards;}
-        &:last-child  { animation: fadefadeInDownIn10 1s ease-in-out 3s; animation-fill-mode:forwards;}
+        &:first-child  { animation: fadefadeInDownIn10 0.7s ease-in-out 2.8s; animation-fill-mode:forwards;}
+        &:last-child  { animation: fadefadeInDownIn10 0.5s ease-in-out 2.8s; animation-fill-mode:forwards;}
 
     }
-    .oStyle {padding: 5px;}
+    .oStyle {padding: 0;height: 20px;padding: 5px;}
     .bold {font-weight: bold;}
 `;
 
@@ -40,13 +39,13 @@ const ProfileNameDetail = ({
         <ProfileNameDetailBlock 
             onAnimationEnd={onAnimationEnd}
         >
-                <p className='oStyle'>OOO<br /></p>
+                <p className='oStyle'>```<br /></p>
                 <p className='bold'>반갑습니다 <br/></p>
                 <p className='bold'>보다 좋은 UX를<br/>
                    만들어 나가고 싶은<br/></p>
                 <p className='bold'>프론트엔드 개발자 <br/>
                    송민형입니다<br/></p>
-                <p className='oStyle'>OOO<br /></p>
+                <p className='oStyle'>```<br /></p>
         </ProfileNameDetailBlock>
     );
 }
