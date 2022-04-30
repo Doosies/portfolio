87d332c5a30f579dd911b51a0e6c4032ab329be5
Doosies <song961003@gmail.com>
2022-04-30 20:03:27 +0900

    메일전송 위한 ui

---
+++ front/src/components/NameBox.tsx
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+interface NameBoxProps {
+    text: string;
+    textSize: string;
+    height: string;
+    children: React.ReactNode;
+}
+
+const NameBoxBlock = styled.div<{height: string}>`
+    position: relative;
+    width: 100%;
+    height: ${({height})=>height};
+    /* margin-top: 50px; */
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:nth-child(1){flex: 1;}
+    &:nth-child(2){flex: 1;}
+    &:nth-child(3){flex: 5;}
+`
+const Line = styled.div`
+    position: absolute;
+    width: 90%;
+    height: 100%;
+    border: 1px solid white;
+    border-radius: 20px;
+    padding: 20px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const Title = styled.div<{textSize: string;}>`
+    position: absolute;
+    padding-left: 5px;
+    padding-right: 5px;
+
+    top: -0.5rem;
+    left: 1.5rem;
+    background-color: #1F1D1E;
+    font-size: ${({textSize})=>textSize};
+`;
+const Content = styled.div`
+    /* position: absolute; */
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const NameBox = ({
+    text, children,textSize, height
+}:NameBoxProps) => {
+    return (
+        <NameBoxBlock height={height}>
+            <Line >
+                <Title textSize={textSize}>
+                    {text}
+                </Title>
+                <Content >
+                    {children}
+                </Content>
+            </Line>
+        </NameBoxBlock>
+    );
+}
+
+export default NameBox;
--- front/src/components/pages/SendMail.tsx
+++ front/src/components/pages/SendMail.tsx
@@ -1,12 +1,58 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import NameBox from '../NameBox';
 
 const SendMailBlock = styled.div`
-    
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
 `
+const Contents = styled.div`
+    width: 100%;
+    height: 90%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 40px;
+
+`;
+
+const Input = styled.input`
+    width: 100%;
+    height: 100%;
+    background-color: #1F1D1E;
+    color: white;
+`;
+const InputArea = styled.textarea`
+    width: 100%;
+    height: 100%;
+    background-color: #1F1D1E;
+    color: white;
+`;
 const SendMail = () => {
+    const text = useState({
+        contact: '',
+        title: '',
+        text: '',
+    })
     return (
         <SendMailBlock>
-            s
+            <NameBox text='메일 입력' textSize='18px' height='90%;'>
+                <Contents>
+                    <NameBox text='연락처' textSize='16px' height='10px'>
+                        <Input />
+                    </NameBox>
+                    <NameBox text='제목' textSize='16px' height='50px'>
+                        <Input />
+                    </NameBox>
+                    <NameBox text='내용' textSize='16px' height='400px'>
+                        <InputArea />
+                    </NameBox>
+                </Contents>
+            </NameBox>
         </SendMailBlock>
     );
 }
